b67070cefe56f2096a722b81449325ba186e235e
aditi3175 <aditigupta3175@gmail.com>
2026-05-09 14:22:53 +0530

    feat: implement layout components for Navbar and Workspace dashboard view

--- devboard-x/src/components/layout/Navbar.jsx
+++ devboard-x/src/components/layout/Navbar.jsx
@@ -1,3 +1,4 @@
+import { Search, Bell, User } from "lucide-react"
 export default function Navbar() {
   return (
     <div className="h-16 w-full bg-zinc-900 text-white px-6 flex items-center justify-between">
@@ -12,12 +13,23 @@ export default function Navbar() {
       {/* Right Side */}
       <div className="flex items-center gap-3">
 
-        <button className="px-4 py-2 rounded-lg hover:bg-zinc-800 transition">
-          Search
+        <div className="flex items-center gap-2 w-80 border border-zinc-700 focus-within:border-zinc-500 px-4 py-2 rounded-lg transition">
+          <Search size={18} className="text-zinc-400" />
+          <input
+            type="text"
+            placeholder="find it here!"
+            className="w-full outline-none bg-transparent text-white text-sm placeholder-zinc-500"
+          />
+        </div>
+
+        <button className="flex items-center gap-2 px-4 py-2 rounded-lg hover:bg-zinc-800 transition">
+          <Bell size={18} />
+          <span>notifications</span>
         </button>
 
-        <button className="px-4 py-2 rounded-lg hover:bg-zinc-800 transition">
-          Profile
+        <button className="flex items-center gap-2 px-4 py-2 rounded-lg hover:bg-zinc-800 transition">
+          <User size={18} />
+          <span>Profile</span>
         </button>
 
       </div>
--- devboard-x/src/components/layout/Workspace.jsx
+++ devboard-x/src/components/layout/Workspace.jsx
@@ -1,8 +1,37 @@
+import { Cpu, Activity, Folder, Server } from "lucide-react";
+import WidgetCard from "@/components/widgets/WidgetCard";
+
 export default function Workspace() {
   return (
-    <div className="flex-1 flex flex-col items-center justify-center bg-zinc-950 text-white p-6">
+    <div className="flex-1 flex flex-col bg-zinc-950 text-white p-6">
       <h1 className="text-4xl font-bold">Welcome to the DevBoard-X</h1>
       <p className="text-zinc-500">Start by creating a new project</p>
+      <div className="grid grid-cols-4 gap-6 mt-6">
+        <WidgetCard
+        Icon={Cpu}
+        title="CPU Usage"
+        value="60%"
+        color="bg-blue-500"
+        />
+        <WidgetCard
+        Icon={Activity}
+        title="Memory Usage"
+        value="80%"
+        color="bg-red-500"
+        />
+        <WidgetCard
+        Icon={Folder}
+        title="Projects"
+        value="12"
+        color="bg-green-500"
+        />
+        <WidgetCard
+        Icon={Server}
+        title="Servers"
+        value="3"
+        color="bg-yellow-500"
+        />
+        </div>
     </div>
   )
 }
